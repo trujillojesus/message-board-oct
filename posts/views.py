ca0fd72f2ecd5f3bd57b1132c6c264d9e30616a9
trujillojesus <trujillo.jesus@yahoo.com>
2024-10-08 18:10:50 -0400

    post list added

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Publication
+from django.views.generic import ListView
 
 # Create your views here.
+class PostListView(ListView):
+    model = Publication
+    template_name = "post_list.html"
